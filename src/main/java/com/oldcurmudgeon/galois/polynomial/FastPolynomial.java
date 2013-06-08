b5540b32ff3aa62ee4616fa3acf858ff808779f9
Paul <CaswellPaulA@AOL.com>
2013-06-08 22:42:05 +0100

    -

---
+++ src/main/java/com/oldcurmudgeon/galois/polynomial/FastPolynomial.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.oldcurmudgeon.galois.polynomial;
+
+import sparsebitmap.SparseBitmap;
+
+/**
+ * @author Expression project.property.user is undefined on line 11, column 14 in Templates/Classes/Class.java.
+ */
+public class FastPolynomial {
+
+  public static void main(String[] args) {
+    SparseBitmap bm = new SparseBitmap();
+  }
+}
